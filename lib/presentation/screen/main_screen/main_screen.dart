a029a552627562ef2a6004f9cf65f4105dfde546
aninarafath6 <arafathanin6@gmail.com>
2022-02-15 15:26:20 +0530

    app bar type changed

--- lib/presentation/screen/main_screen/main_screen.dart
+++ lib/presentation/screen/main_screen/main_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:netflix_clone/core/sizes/app_sizes.dart';
+import 'package:netflix_clone/core/strings/downloads_page/downloads_strings.dart';
 import 'package:netflix_clone/presentation/screen/main_screen/widgets/bottom_nav.dart';
 import 'package:netflix_clone/presentation/screen/pages/downloads_page/downloads_page.dart';
 import 'package:netflix_clone/presentation/screen/pages/home_page/home_page.dart';
@@ -6,8 +8,10 @@ import 'package:netflix_clone/presentation/screen/pages/laughs_page/laughs_page.
 import 'package:netflix_clone/presentation/screen/pages/new_and_hot_page/new_and_hot_page.dart';
 import 'package:netflix_clone/presentation/screen/pages/search_page/search_page.dart';
 
+import '../../widgets/app_bar_widget.dart';
+
 class MainScreen extends StatelessWidget {
-   MainScreen({Key? key}) : super(key: key);
+  const MainScreen({Key? key}) : super(key: key);
 
   final List<Widget> pages = const [
     HomePage(),
@@ -17,9 +21,23 @@ class MainScreen extends StatelessWidget {
     DownloadsPage(),
   ];
 
+  final List<String> pageTitles = const [
+    'Home',
+    'New & Hot',
+    'Fast Laughs',
+    'Search',
+    'Downloads',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: const PreferredSize(
+        preferredSize: Size.fromHeight(AppSizes.toolbarHeight),
+        child: CAppBar(
+          title: DownloadsStrings.downloadsTitle,
+        ),
+      ),
       body: PageView.builder(
         itemCount: pages.length,
         itemBuilder: (context, index) => pages[index],
--- lib/presentation/screen/pages/downloads_page/downloads_page.dart
+++ lib/presentation/screen/pages/downloads_page/downloads_page.dart
@@ -1,11 +1,9 @@
-
 import 'package:flutter/material.dart';
 import 'package:netflix_clone/core/colors/app_colors.dart';
 import 'package:netflix_clone/core/sizes/app_sizes.dart';
 import 'package:netflix_clone/core/strings/downloads_page/downloads_strings.dart';
 import 'package:netflix_clone/presentation/screen/pages/downloads_page/widgets/downloads_banner_widget.dart';
 import 'package:netflix_clone/presentation/screen/pages/downloads_page/widgets/sub_title_row_widget.dart';
-import 'package:netflix_clone/presentation/widgets/app_bar_widget.dart';
 
 import 'widgets/action_button_widget.dart';
 
@@ -14,48 +12,40 @@ class DownloadsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const PreferredSize(
-        preferredSize: Size.fromHeight(AppSizes.toolbarHeight),
-        child: CAppBar(
-          title: DownloadsStrings.downloadsTitle,
+    return ListView(
+      children: const [
+        SubTitleRow(),
+        AppSizes.lgVSpacer,
+        Text(
+          DownloadsStrings.idu,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: AppSizes.titleFontSize - 4,
+            fontWeight: FontWeight.bold,
+          ),
         ),
-      ),
-      body: ListView(
-        children: const [
-          SubTitleRow(),
-          AppSizes.lgVSpacer,
-          Text(
-            DownloadsStrings.idu,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: AppSizes.titleFontSize - 4,
-              fontWeight: FontWeight.bold,
-            ),
+        AppSizes.smVSpacer,
+        Text(
+          DownloadsStrings.description,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 16,
+            color: Colors.grey,
           ),
-          AppSizes.smVSpacer,
-          Text(
-            DownloadsStrings.description,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 16,
-              color: Colors.grey,
-            ),
-          ),
-          AppSizes.smVSpacer,
-          DownloadsBannerWidget(),
-          AppSizes.lgVSpacer,
-          ActionButtons(
-            label: 'Set Up',
-            color: AppColors.buttonColor,
-          ),
-          AppSizes.smVSpacer,
-          ActionButtons(
-            label: 'See What You Can Download',
-            isSmall: true,
-          )
-        ],
-      ),
+        ),
+        AppSizes.smVSpacer,
+        DownloadsBannerWidget(),
+        AppSizes.lgVSpacer,
+        ActionButtons(
+          label: 'Set Up',
+          color: AppColors.buttonColor,
+        ),
+        AppSizes.smVSpacer,
+        ActionButtons(
+          label: 'See What You Can Download',
+          isSmall: true,
+        )
+      ],
     );
   }
 }
